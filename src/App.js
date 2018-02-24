02a63affbc68f2a7e33557dfe8c1eb98a0bf68b2
Christopher Fox <cfox1290@gmail.com>
2018-02-23 17:10:28 -0700

    adding pre state

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import game from './Game'
+
+class App extends Component {
+  render() {
+    game();
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
